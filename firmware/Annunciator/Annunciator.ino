f2a1bd17b92bb927af9d2ff8ebd077d4c254717c
tsaG1337 <tsaG1337@users.noreply.github.com>
2018-06-13 11:45:20 +0200

    Midnight Update
    Added Support for Standard NMEA Checksum
    Rewrote the LED Set method
    LEDs will only be updated when there is a new LED Sentence received
    Fixed a bug in the Sentence encoding which occasionally failed to recognize the sentence
    Changed to correct Baudrate (230400)
    Added a timeout, if no data is received for 1500mS, all LEDs will turn blue (currently using the fuelLH field for it. Since one sentence contains every value there should be no problem)

--- firmware/Annunciator/Annunciator.ino
+++ firmware/Annunciator/Annunciator.ino
@@ -19,38 +19,37 @@ int rpmRange[] = {0, 1440, 1800, 5500, 5800, 5850};
 // Red_end , yellow_begin, green_begin, green_end, yellow_end, red_begin) (1/10 Bar)
 int oilPressRange[] = { 1 , 15 , 15 , 50 , 50 , 70};
 int oilTempRange[] = { 1 , 50 , 90 , 110 , 110 , 140};
-int egtRange[] = { 880};
+int egtRange[] = { 500, 800, 880};
 int CHTTempRange[] = {120, 150};
 int fuelPressureRange[] = {40, 150}; // ( 1/100 Bar)
 int fuelLHRange[] = {0, 200, 400};
 int fuelRHRange[] = {0, 200, 400};
-int voltsRange[] = {10, 14};
+int voltsRange[] = {100, 140};
 
-int lastUpdate = 0;
-int updateTimeout = 100;
-char sentence[7] = "$ICMB912";
+
+
 
 //***Linking TinyGPS with the incoming data***//
-TinyGPSCustom RPM(gps, sentence, 1);
-TinyGPSCustom manPress(gps, sentence, 2);
-TinyGPSCustom oilPress(gps, sentence, 3);
-TinyGPSCustom fuelPress(gps, sentence, 4);
-TinyGPSCustom oilTemp(gps, sentence, 5);
-TinyGPSCustom CHT1(gps, sentence, 6);
-TinyGPSCustom CHT2(gps, sentence, 7);
-TinyGPSCustom OAT(gps, sentence, 8);
-TinyGPSCustom fuelLH(gps, sentence, 9);
-TinyGPSCustom fuelRH(gps, sentence, 10);
-TinyGPSCustom fuelFlow(gps, sentence, 11);
-TinyGPSCustom fuelSum(gps, sentence, 12);
-TinyGPSCustom EGT1(gps, sentence, 13);
-TinyGPSCustom EGT2(gps, sentence, 14);
-TinyGPSCustom flaps(gps, sentence, 15);
-TinyGPSCustom volts(gps, sentence, 16);
-TinyGPSCustom amps(gps, sentence, 17);
+TinyGPSCustom RPM(gps, "ICMB912", 1);
+TinyGPSCustom manPress(gps, "ICMB912", 2);
+TinyGPSCustom oilPress(gps, "ICMB912", 3);
+TinyGPSCustom fuelPress(gps, "ICMB912", 4);
+TinyGPSCustom oilTemp(gps, "ICMB912", 5);
+TinyGPSCustom CHT1(gps, "ICMB912", 6);
+TinyGPSCustom CHT2(gps, "ICMB912", 7);
+TinyGPSCustom OAT(gps, "ICMB912", 8);
+TinyGPSCustom fuelLH(gps, "ICMB912", 9);
+TinyGPSCustom fuelRH(gps, "ICMB912", 10);
+TinyGPSCustom fuelFlow(gps, "ICMB912", 11);
+TinyGPSCustom fuelSum(gps, "ICMB912", 12);
+TinyGPSCustom EGT1(gps, "ICMB912", 13);
+TinyGPSCustom EGT2(gps, "ICMB912", 14);
+TinyGPSCustom flaps(gps, "ICMB912", 15);
+TinyGPSCustom volts(gps, "ICMB912", 16);
+TinyGPSCustom amps(gps, "ICMB912", 17);
 
 void setup() {
-  Serial.begin(4800);
+  Serial.begin(230400);
 
   //Init Shiftregisters
   ShiftPWM.SetAmountOfRegisters(numRegisters);
@@ -63,24 +62,35 @@ void setup() {
     ShiftPWM.SetAllHSV(hue, 255, 255);
     delay(5);
   }
-  ShiftPWM.SetAll(0);
+  for (int led = 0; led < numRGBleds; led++) {
+      ShiftPWM.SetRGB(led, 0, 0, currBrightness); // blue
+    }
   //End init Shiftregisters
 }
 
 
 void loop() {
+
   while (Serial.available() > 0) {
-    gps.encode(Serial.read());
-    lastUpdate = millis();
+    char incomingByte = Serial.read();
+    gps.encode(incomingByte);
+  }
+  bool updated = fuelLH.isUpdated();
+  if (updated) {
+    setLED();
   }
 
-  setLED();
+  if (fuelLH.age() >= 1500) {
+    for (int led = 0; led < numRGBleds; led++) {
+      ShiftPWM.SetRGB(led, 0, 0, currBrightness); // blue
+    }
+  }
 }
 
 void setLED() {
   //Set Oil Pressure LED
   //int oilPressRange[] = { 0 , 15 , 15 , 50 , 50 , 70};
-  int currOilPress = oilPress.value();
+  int currOilPress = atoi(oilPress.value());
 
   if ((currOilPress < oilPressRange[1] && currOilPress > oilPressRange[0] ) ||  currOilPress > oilPressRange[4]) {
     ShiftPWM.SetRGB(7, currBrightness, 0, 0); //red
@@ -94,84 +104,81 @@ void setLED() {
 
   //Set Fuel LH LED
   //int fuelLHRange[] = {0, 200, 400};
-  int currfuelLH = fuelLH.value();
-  if ((currfuelLH < fuelLHRange[1] && currfuelLH > 0 ) ) {
+  int currfuelLH = atoi(fuelLH.value());
+  if (currfuelLH <= fuelLHRange[1] ) {
     ShiftPWM.SetRGB(6, currBrightness, 0, 0); //red
-  } else if (currfuelLH >= fuelLHRange[1] &&  currfuelLH <= fuelLHRange[2]) {
+  } else  {
     ShiftPWM.SetRGB(6, 0, currBrightness, 0); //green
-  } else {
-    ShiftPWM.SetRGB(6, currBrightness, 0, currBrightness); // purple
   }
   //End Fuel LH
 
 
   //Set Oil Temp LED
   //int oilTempRange[] = { 0 , 50 , 90 , 110 , 110 , 140};
-  int currOilTemp = oilTemp.value();
-  if ((currOilTemp < oilTempRange[1] && currOilTemp > 0 ) ||  (currOilTemp > oilTempRange[5] && currOilTemp > 0 )) {
+  int currOilTemp = atoi(oilTemp.value());
+  if ((currOilTemp < oilTempRange[1] ) ||  (currOilTemp > oilTempRange[5] )) {
     ShiftPWM.SetRGB(5, currBrightness, 0, 0); //red
   } else if (currOilTemp >= oilTempRange[2] &&  currOilTemp <= oilTempRange[3]) {
     ShiftPWM.SetRGB(5, 0, currBrightness, 0); //green
-  } else {
-    ShiftPWM.SetRGB(5, currBrightness, 0, currBrightness); // purple
   }
   //End Oil Temp
 
-  int thisUpdate = millis();
-  if (thisUpdate - lastUpdate <= updateTimeout) {
+
+  //Set Fuel Pressure LED
+  //int fuelPressureRange[] = {40, 150};
+  int currFuelPress = atoi(fuelPress.value());
+  if ((currFuelPress < fuelPressureRange[0] ) ||  (currFuelPress > fuelPressureRange[1] )) {
+    ShiftPWM.SetRGB(4, currBrightness, 0, 0); //red
+  } else if (currFuelPress >= fuelPressureRange[0] &&  currFuelPress <= fuelPressureRange[1]) {
     ShiftPWM.SetRGB(4, 0, currBrightness, 0); //green
-  } else {
-    ShiftPWM.SetRGB(4, currBrightness, 0, 0);//red
   }
+  //End Fuel Pressure
 
 
 
   //Set Volts LED
-  //int voltsRange[] = {10,14};
-  int currVolts = volts.value();
-  if ((currVolts < voltsRange[0] && currVolts > 0 ) ||  (currVolts > voltsRange[1] && currVolts > 0 )) {
+  //int voltsRange[] = {100,140};
+  int currVolts = atoi(volts.value());
+  if ((currVolts < voltsRange[0] ) ||  (currVolts > voltsRange[1] )) {
     ShiftPWM.SetRGB(3, currBrightness, 0, 0); //red
-  } else if (currVolts >= voltsRange[0] &&  voltsRange <= voltsRange[1]) {
+  } else if (currVolts >= voltsRange[0] &&  currVolts <= voltsRange[1]) {
     ShiftPWM.SetRGB(3, 0, currBrightness, 0); //green
-  } else {
-    ShiftPWM.SetRGB(3, currBrightness, 0, currBrightness); // purple
   }
   //End Volts
 
 
   //Set CHT Temp LED
-  //int CHTTempRange[] = {120, 150};
-  int currCHTTemp = CHT1.value();
-  if ((currCHTTemp < CHTTempRange[0] && currOilTemp > 0 ) ||  (currCHTTemp > CHTTempRange[1] && currCHTTemp > 0 )) {
+  //int CHTTempRange[] = {60, 150};
+  int currCHTTemp = atoi(CHT1.value());
+  if ((currCHTTemp < CHTTempRange[0] ) ||  (currCHTTemp > CHTTempRange[1] )) {
     ShiftPWM.SetRGB(2, currBrightness, 0, 0); //red
   } else if (currCHTTemp >= CHTTempRange[0] &&  currCHTTemp <= CHTTempRange[1]) {
     ShiftPWM.SetRGB(2, 0, currBrightness, 0); //green
-  } else {
-    ShiftPWM.SetRGB(2, currBrightness, 0, currBrightness); // purple
   }
   //End CHT Temp
 
 
   //Set Fuel RH LED
   //int fuelRHRange[] = {0, 200, 400};
-  int currfuelRH = fuelRH.value();
-  if ((currfuelRH < fuelRHRange[1] && currfuelRH > 1 ) ) {
+
+  int currfuelRH = atoi(fuelRH.value());
+  if (currfuelRH <= fuelRHRange[1] ) {
     ShiftPWM.SetRGB(1, currBrightness, 0, 0); //red
-  } else if (currfuelRH >= fuelRHRange[1] &&  currfuelRH <= fuelRHRange[2]) {
+  } else  {
     ShiftPWM.SetRGB(1, 0, currBrightness, 0); //green
-  } else {
-    ShiftPWM.SetRGB(1, currBrightness, 0, currBrightness); // purple
   }
   //End Fuel RH
 
 
   //Set EGT LED
-  //int egtRange[] = { 880};
-  int currEGT = EGT1.value();
-  if ((currEGT <= egtRange[1]) ) {
-    ShiftPWM.SetRGB(0, 0, currBrightness, 0); //green
+  //int egtRange[] = { 500, 800, 880};
+  int currEGT = atoi(EGT1.value());
+  if ((currEGT <= egtRange[0]) ) {
+    ShiftPWM.SetRGB(0, currBrightness, currBrightness, 0); //yellow
+  } else  if ((currEGT >= egtRange[2]) ) {
+    ShiftPWM.SetRGB(0, currBrightness, 0, 0); //red
   } else {
-    ShiftPWM.SetRGB(0, currBrightness, 0, 0); //green
+    ShiftPWM.SetRGB(0, 0, currBrightness, 0); //green
   }
   //End EGT
 }
