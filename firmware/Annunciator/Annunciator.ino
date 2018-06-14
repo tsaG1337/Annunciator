1386be7b1c062c3afac21c01d946eb01d93f2c01
tsaG1337 <tsaG1337@users.noreply.github.com>
2018-06-15 01:25:39 +0200

    Added FAKE NMEA decoder
    Added own Fake NMEA decoder since the iCFly does not use a Checksum or even an Asterics....hmpf.
    -removed tinyGPS

--- firmware/Annunciator/Annunciator.ino
+++ firmware/Annunciator/Annunciator.ino
@@ -1,7 +1,4 @@
-#include "TinyGPS++.h"
-TinyGPSPlus gps;
-
-//ShiftPWM Setup
+//*** ShiftPWM Setup ***//
 const int ShiftPWM_latchPin = 9; //Latch Pin for Shift-Register
 const bool ShiftPWM_invertOutputs = true; //Invert Outputs
 const bool ShiftPWM_balanceLoad = true; //If true, only one Output will be on at the time (Power Saving)
@@ -13,7 +10,7 @@ int numRegisters = 3;
 int numRGBleds = numRegisters * 8 / 3;
 int currBrightness = 128;
 
-//***Definition of operational ranges***//
+//*** Definition of operational ranges ***//
 
 int rpmRange[] = {0, 1440, 1800, 5500, 5800, 5850};
 // Red_end , yellow_begin, green_begin, green_end, yellow_end, red_begin) (1/10 Bar)
@@ -26,27 +23,13 @@ int fuelLHRange[] = {0, 200, 400};
 int fuelRHRange[] = {0, 200, 400};
 int voltsRange[] = {100, 140};
 
-
-
-
-//***Linking TinyGPS with the incoming data***//
-TinyGPSCustom RPM(gps, "ICMB912", 1);
-TinyGPSCustom manPress(gps, "ICMB912", 2);
-TinyGPSCustom oilPress(gps, "ICMB912", 3);
-TinyGPSCustom fuelPress(gps, "ICMB912", 4);
-TinyGPSCustom oilTemp(gps, "ICMB912", 5);
-TinyGPSCustom CHT1(gps, "ICMB912", 6);
-TinyGPSCustom CHT2(gps, "ICMB912", 7);
-TinyGPSCustom OAT(gps, "ICMB912", 8);
-TinyGPSCustom fuelLH(gps, "ICMB912", 9);
-TinyGPSCustom fuelRH(gps, "ICMB912", 10);
-TinyGPSCustom fuelFlow(gps, "ICMB912", 11);
-TinyGPSCustom fuelSum(gps, "ICMB912", 12);
-TinyGPSCustom EGT1(gps, "ICMB912", 13);
-TinyGPSCustom EGT2(gps, "ICMB912", 14);
-TinyGPSCustom flaps(gps, "ICMB912", 15);
-TinyGPSCustom volts(gps, "ICMB912", 16);
-TinyGPSCustom amps(gps, "ICMB912", 17);
+//*** Settings for FAKE NMEA Decoder ***//
+enum values { RPM, manPress, oilPress, fuelPress, oilTemp, CHT1, CHT2, OAT, fuelLH, fuelRH, fuelFlow, fuelSum, EGT1, EGT2, flaps, volts, amps };
+bool dataUpdated = 0;
+int16_t valueArray[20];
+unsigned long lastUpdatedMillis = 0;
+unsigned long previousMillis = 0;
+unsigned long timeOutIntervall = 1500;
 
 void setup() {
   Serial.begin(230400);
@@ -55,6 +38,7 @@ void setup() {
   ShiftPWM.SetAmountOfRegisters(numRegisters);
   ShiftPWM.SetPinGrouping(1);
   ShiftPWM.Start(pwmFrequency, maxBrightness);
+  
   // Turn all LED's off.
   ShiftPWM.SetAll(0);
 
@@ -62,9 +46,10 @@ void setup() {
     ShiftPWM.SetAllHSV(hue, 255, 255);
     delay(5);
   }
+  //Set all LEDs to blue
   for (int led = 0; led < numRGBleds; led++) {
-      ShiftPWM.SetRGB(led, 0, 0, currBrightness); // blue
-    }
+    ShiftPWM.SetRGB(led, 0, 0, currBrightness); // blue
+  }
   //End init Shiftregisters
 }
 
@@ -73,14 +58,15 @@ void loop() {
 
   while (Serial.available() > 0) {
     char incomingByte = Serial.read();
-    gps.encode(incomingByte);
-  }
-  bool updated = fuelLH.isUpdated();
-  if (updated) {
-    setLED();
+    readIncomingData(incomingByte);
   }
 
-  if (fuelLH.age() >= 1500) {
+  if (dataUpdated) {
+    setLED();
+    dataUpdated = 0;
+  }
+  unsigned long currentmillis = millis();
+  if (currentmillis - lastUpdatedMillis >= 1500) {
     for (int led = 0; led < numRGBleds; led++) {
       ShiftPWM.SetRGB(led, 0, 0, currBrightness); // blue
     }
@@ -90,7 +76,7 @@ void loop() {
 void setLED() {
   //Set Oil Pressure LED
   //int oilPressRange[] = { 0 , 15 , 15 , 50 , 50 , 70};
-  int currOilPress = atoi(oilPress.value());
+  int currOilPress = valueArray[oilPress];
 
   if ((currOilPress < oilPressRange[1] && currOilPress > oilPressRange[0] ) ||  currOilPress > oilPressRange[4]) {
     ShiftPWM.SetRGB(7, currBrightness, 0, 0); //red
@@ -104,7 +90,7 @@ void setLED() {
 
   //Set Fuel LH LED
   //int fuelLHRange[] = {0, 200, 400};
-  int currfuelLH = atoi(fuelLH.value());
+  int currfuelLH = valueArray[fuelLH];
   if (currfuelLH <= fuelLHRange[1] ) {
     ShiftPWM.SetRGB(6, currBrightness, 0, 0); //red
   } else  {
@@ -115,7 +101,7 @@ void setLED() {
 
   //Set Oil Temp LED
   //int oilTempRange[] = { 0 , 50 , 90 , 110 , 110 , 140};
-  int currOilTemp = atoi(oilTemp.value());
+  int currOilTemp = valueArray[oilTemp];
   if ((currOilTemp < oilTempRange[1] ) ||  (currOilTemp > oilTempRange[5] )) {
     ShiftPWM.SetRGB(5, currBrightness, 0, 0); //red
   } else if (currOilTemp >= oilTempRange[2] &&  currOilTemp <= oilTempRange[3]) {
@@ -126,7 +112,7 @@ void setLED() {
 
   //Set Fuel Pressure LED
   //int fuelPressureRange[] = {40, 150};
-  int currFuelPress = atoi(fuelPress.value());
+  int currFuelPress = valueArray[fuelPress];
   if ((currFuelPress < fuelPressureRange[0] ) ||  (currFuelPress > fuelPressureRange[1] )) {
     ShiftPWM.SetRGB(4, currBrightness, 0, 0); //red
   } else if (currFuelPress >= fuelPressureRange[0] &&  currFuelPress <= fuelPressureRange[1]) {
@@ -135,10 +121,9 @@ void setLED() {
   //End Fuel Pressure
 
 
-
   //Set Volts LED
   //int voltsRange[] = {100,140};
-  int currVolts = atoi(volts.value());
+  int currVolts = valueArray[volts];
   if ((currVolts < voltsRange[0] ) ||  (currVolts > voltsRange[1] )) {
     ShiftPWM.SetRGB(3, currBrightness, 0, 0); //red
   } else if (currVolts >= voltsRange[0] &&  currVolts <= voltsRange[1]) {
@@ -149,7 +134,7 @@ void setLED() {
 
   //Set CHT Temp LED
   //int CHTTempRange[] = {60, 150};
-  int currCHTTemp = atoi(CHT1.value());
+  int currCHTTemp = valueArray[CHT1];
   if ((currCHTTemp < CHTTempRange[0] ) ||  (currCHTTemp > CHTTempRange[1] )) {
     ShiftPWM.SetRGB(2, currBrightness, 0, 0); //red
   } else if (currCHTTemp >= CHTTempRange[0] &&  currCHTTemp <= CHTTempRange[1]) {
@@ -161,7 +146,7 @@ void setLED() {
   //Set Fuel RH LED
   //int fuelRHRange[] = {0, 200, 400};
 
-  int currfuelRH = atoi(fuelRH.value());
+  int currfuelRH = valueArray[fuelRH];
   if (currfuelRH <= fuelRHRange[1] ) {
     ShiftPWM.SetRGB(1, currBrightness, 0, 0); //red
   } else  {
@@ -172,7 +157,7 @@ void setLED() {
 
   //Set EGT LED
   //int egtRange[] = { 500, 800, 880};
-  int currEGT = atoi(EGT1.value());
+  int currEGT = valueArray[EGT1];
   if ((currEGT <= egtRange[0]) ) {
     ShiftPWM.SetRGB(0, currBrightness, currBrightness, 0); //yellow
   } else  if ((currEGT >= egtRange[2]) ) {
@@ -181,4 +166,51 @@ void setLED() {
     ShiftPWM.SetRGB(0, 0, currBrightness, 0); //green
   }
   //End EGT
+}
+
+void readIncomingData(char ByteRead) {
+  static char dataArray[100];
+  static byte valuePointer = 0;
+  static byte dataPointer = 0;
+  static byte dataBeginPointer = 0;
+  if (ByteRead == '$') {
+    memset(valueArray, 0, sizeof(valueArray));
+    memset(dataArray, 0, sizeof(dataArray));
+    dataBeginPointer = 0;
+    dataPointer = 0;
+    valuePointer = 0;
+    dataArray[dataPointer] = ByteRead;
+    dataPointer++;
+    dataUpdated = 1;
+    lastUpdatedMillis = millis();
+
+  } else if (ByteRead == ',') {
+    int dataValue = 0;
+    dataValue = convert_slice(dataArray, dataBeginPointer, dataPointer);
+    valueArray[valuePointer] = dataValue;
+    valuePointer++;
+    dataBeginPointer = dataPointer;
+
+  } else if (ByteRead == '\r') {
+    int dataValue = 0;
+    dataValue = convert_slice(dataArray, dataBeginPointer, dataPointer);
+    valueArray[valuePointer] = dataValue;
+    valuePointer++;
+    dataBeginPointer = dataPointer;
+  } else if (ByteRead == '\n') {
+    Serial.println("END");
+
+  } else {
+    dataArray[dataPointer] = ByteRead;
+    dataPointer++;
+  }
+
+}
+
+int convert_slice(const char *s, size_t a, size_t b) {
+  int val = 0;
+  while (a < b) {
+    val = val * 10 + s[a++] - '0';
+  }
+  return val;
 }
